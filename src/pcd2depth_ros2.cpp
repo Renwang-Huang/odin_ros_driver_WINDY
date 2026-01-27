8a64e553f9aabea6e4d4d5d4a0d14c4bb3999c1d
Renwang Huang <RenwangHuangX@gmail.com>
2026-01-27 20:39:48 +0800

    使用绝对路径查找calib.yaml

--- src/pcd2depth_ros2.cpp
+++ src/pcd2depth_ros2.cpp
@@ -1,16 +1,3 @@
-/*
-Copyright 2025 Manifold Tech Ltd.(www.manifoldtech.com.co)
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-   http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 #include <rclcpp/rclcpp.hpp>
 #include <fstream>
 #include <sys/stat.h>
@@ -20,6 +7,7 @@ limitations under the License.
 #include <string>
 #include "depth_image_ros2_node.hpp"
 #include <rcpputils/filesystem_helper.hpp>
+
 bool fileExists(const std::string& filename) {
     struct stat buffer;
     return (stat(filename.c_str(), &buffer) == 0);
@@ -166,7 +154,8 @@ int main(int argc, char **argv)
         return 0;
     }
     
-    std::string calib_file_path = node->declare_parameter<std::string>("calib_file_path", "");
+    // std::string calib_file_path = node->declare_parameter<std::string>("calib_file_path", "");
+    std::string calib_file_path = "/home/renwang/workspace_humble/src/odin_ros_driver/config/calib.yaml";
     
     RCLCPP_INFO(node->get_logger(), "Waiting for calib.yaml file at: %s", calib_file_path.c_str());
     while(rclcpp::ok() && !fileExists(calib_file_path))
